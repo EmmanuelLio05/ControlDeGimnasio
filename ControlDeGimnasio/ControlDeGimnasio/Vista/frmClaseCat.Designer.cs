3f0839e0c18af5e116348214f9cf6f2630390b51
EmmanuelLio05 <70165675+EmmanuelLio05@users.noreply.github.com>
2021-06-24 09:41:30 -0500

    [UPDATE] Catalogo clase

--- ControlDeGimnasio/ControlDeGimnasio/Vista/frmClaseCat.Designer.cs
+++ ControlDeGimnasio/ControlDeGimnasio/Vista/frmClaseCat.Designer.cs
@@ -24,19 +24,29 @@ namespace ControlDeGimnasio.Vista {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
             this.dgvClases = new System.Windows.Forms.DataGridView();
+            this.bsClase = new System.Windows.Forms.BindingSource(this.components);
+            this.descripcionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.horaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.entrenadorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnActulizar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClases)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsClase)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.btnActulizar);
             this.panel1.Controls.Add(this.btnSalir);
-            this.panel1.Controls.Add(this.btnGuardar);
+            this.panel1.Controls.Add(this.btnRegistrar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -45,46 +55,102 @@ namespace ControlDeGimnasio.Vista {
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(3, 56);
+            this.btnSalir.Location = new System.Drawing.Point(3, 94);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(39, 23);
             this.btnSalir.TabIndex = 1;
             this.btnSalir.Text = "button2";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
-            // btnGuardar
+            // btnRegistrar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(3, 14);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(39, 23);
-            this.btnGuardar.TabIndex = 0;
-            this.btnGuardar.Text = "button1";
-            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Location = new System.Drawing.Point(3, 14);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(39, 23);
+            this.btnRegistrar.TabIndex = 0;
+            this.btnRegistrar.Text = "button1";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
             // dgvClases
             // 
+            this.dgvClases.AllowUserToAddRows = false;
+            this.dgvClases.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.dgvClases.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvClases.AutoGenerateColumns = false;
             this.dgvClases.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClases.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.descripcionDataGridViewTextBoxColumn,
+            this.horaDataGridViewTextBoxColumn,
+            this.nombreDataGridViewTextBoxColumn,
+            this.entrenadorDataGridViewTextBoxColumn});
+            this.dgvClases.DataSource = this.bsClase;
             this.dgvClases.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvClases.Location = new System.Drawing.Point(45, 0);
+            this.dgvClases.MultiSelect = false;
             this.dgvClases.Name = "dgvClases";
-            this.dgvClases.Size = new System.Drawing.Size(730, 514);
+            this.dgvClases.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvClases.Size = new System.Drawing.Size(491, 514);
             this.dgvClases.TabIndex = 2;
             this.dgvClases.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClases_CellContentClick);
+            // 
+            // bsClase
+            // 
+            this.bsClase.DataSource = typeof(ControlDeGimnasio.Modelo.Entidades.Clase);
+            // 
+            // descripcionDataGridViewTextBoxColumn
+            // 
+            this.descripcionDataGridViewTextBoxColumn.DataPropertyName = "Descripcion";
+            this.descripcionDataGridViewTextBoxColumn.HeaderText = "Descripcion";
+            this.descripcionDataGridViewTextBoxColumn.Name = "descripcionDataGridViewTextBoxColumn";
+            // 
+            // horaDataGridViewTextBoxColumn
+            // 
+            this.horaDataGridViewTextBoxColumn.DataPropertyName = "Hora";
+            this.horaDataGridViewTextBoxColumn.HeaderText = "Hora";
+            this.horaDataGridViewTextBoxColumn.Name = "horaDataGridViewTextBoxColumn";
+            // 
+            // nombreDataGridViewTextBoxColumn
+            // 
+            this.nombreDataGridViewTextBoxColumn.DataPropertyName = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.HeaderText = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
+            // 
+            // entrenadorDataGridViewTextBoxColumn
+            // 
+            this.entrenadorDataGridViewTextBoxColumn.DataPropertyName = "Entrenador";
+            this.entrenadorDataGridViewTextBoxColumn.HeaderText = "Entrenador";
+            this.entrenadorDataGridViewTextBoxColumn.Name = "entrenadorDataGridViewTextBoxColumn";
+            // 
+            // btnActulizar
+            // 
+            this.btnActulizar.Location = new System.Drawing.Point(3, 53);
+            this.btnActulizar.Name = "btnActulizar";
+            this.btnActulizar.Size = new System.Drawing.Size(39, 23);
+            this.btnActulizar.TabIndex = 2;
+            this.btnActulizar.Text = "button2";
+            this.btnActulizar.UseVisualStyleBackColor = true;
+            this.btnActulizar.Click += new System.EventHandler(this.btnActulizar_Click);
             // 
             // frmClaseCat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 514);
+            this.ClientSize = new System.Drawing.Size(536, 514);
             this.Controls.Add(this.dgvClases);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmClaseCat";
             this.Text = "Catalogo de clases";
             this.Load += new System.EventHandler(this.frmClaseCat_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmClaseCat_KeyDown);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvClases)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsClase)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -93,7 +159,13 @@ namespace ControlDeGimnasio.Vista {
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnRegistrar;
         private System.Windows.Forms.DataGridView dgvClases;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn horaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn entrenadorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource bsClase;
+        private System.Windows.Forms.Button btnActulizar;
     }
 }
--- ControlDeGimnasio/ControlDeGimnasio/Vista/frmClaseCat.cs
+++ ControlDeGimnasio/ControlDeGimnasio/Vista/frmClaseCat.cs
@@ -10,18 +10,49 @@ using System.Windows.Forms;
 
 namespace ControlDeGimnasio.Vista {
     public partial class frmClaseCat : Form {
+        private Modelo.Datos.Clase oDClase = new Modelo.Datos.Clase();
+        private Modelo.Entidades.Clases oEClases = new Modelo.Entidades.Clases();
+
         public frmClaseCat() {
             InitializeComponent();
         }
 
-        private void frmClaseCat_Load(object sender, EventArgs e)
-        {
+        #region Metodos
 
+        #endregion
+
+        #region Eventos
+        private void frmClaseCat_Load(object sender, EventArgs e) {
+            oEClases = oDClase.GetAll();
+            dgvClases.DataSource = oEClases;
         }
 
-        private void dgvClases_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
+        private void btnActulizar_Click(object sender, EventArgs e) {
+            frmClase f = new frmClase();
+            
+            f.Accion = Modelo.Entidades.Acciones.Actualizar;
+            f.Clase = Convert.ToString( dgvClases.SelectedRows[0].Cells["Nombre"].Value);
+            f.ShowDialog();
+        }
 
+        private void btnSalir_Click(object sender, EventArgs e) {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e) {
+            frmClase f = new frmClase();
+            
+            f.Accion = Modelo.Entidades.Acciones.Registrar;
+            f.ShowDialog();
+        }
+        #endregion
+
+        private void frmClaseCat_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Escape) {
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            }
         }
     }
 }
