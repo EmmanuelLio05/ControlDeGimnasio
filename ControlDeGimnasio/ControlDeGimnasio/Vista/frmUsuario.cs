f50e18cf0b5be0d0ee0b3c3c9c7123f4fbf2e0cc
AngelEMR21 <angele-mr@hotmail.com>
2021-06-25 14:37:28 -0500

    [UPDATE] Pantallas de usuario
    Se modificaron las pantallas de usuario

--- ControlDeGimnasio/ControlDeGimnasio/Vista/frmUsuario.cs
+++ ControlDeGimnasio/ControlDeGimnasio/Vista/frmUsuario.cs
@@ -21,5 +21,15 @@ namespace ControlDeGimnasio.Vista
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
